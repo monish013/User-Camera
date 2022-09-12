d6b80b40519f574757c76b9be0137195c9709db5
kravmaguy <37171082+KravMaguy@users.noreply.github.com>
2022-09-12 17:38:01 -0400

    add Html Photo capture input

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className='App'>
+      <label htmlFor='capture'>Capture Photo</label>
+      <div>
+        <input type='file' id='capture' accept='image/*' capture />
+      </div>
+      <div>
+        <img src='' id='img' alt='from phone' />
+      </div>
+    </main>
   );
 }
 
