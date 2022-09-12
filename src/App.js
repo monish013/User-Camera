73fd4e25a25f6f0ab0648c7e0b9da9355fc05e34
kravmaguy <37171082+KravMaguy@users.noreply.github.com>
2022-09-12 18:37:04 -0400

    hide Image component if no file

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <main className='App'>
       <Capture setFile={setFile} />
-      <Image file={file} />
+      {file ? <Image file={file} /> : null}
     </main>
   );
 }
